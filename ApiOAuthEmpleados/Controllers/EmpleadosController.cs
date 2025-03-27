80d14d1dc7d73f058b2840175c6fb43fcb0c5ffd
Alumnos MCSD Mañana <Alumnos MCSD Maana@T06W13>
2025-03-27 10:52:01 +0100

    Add project files.

---
+++ ApiOAuthEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,32 @@
+﻿using ApiOAuthEmpleados.Models;
+using ApiOAuthEmpleados.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiOAuthEmpleados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+        private RepositoryHospital repo;
+
+        public EmpleadosController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Empleado>>> GetEmpleado()
+        {
+            return await this.repo.GetEmpleadosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empleado>>
+            FindEmpleado(int id)
+        {
+            return await this.repo.FindEmpleado(id);
+        }
+    }
+}
---
+++ ApiOAuthEmpleados/Program.cs
@@ -0,0 +1,45 @@
+using ApiOAuthEmpleados.Context;
+using ApiOAuthEmpleados.Helpers;
+using ApiOAuthEmpleados.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+HelperActionServicesOAuth helper =
+    new HelperActionServicesOAuth(builder.Configuration);
+
+builder.Services.AddSingleton<HelperActionServicesOAuth>(helper);
+builder.Services.AddAuthentication(helper.GetAuthenticateSchema())
+    .AddJwtBearer(helper.GetJwtBearerOptions());
+
+// Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("Sql");
+builder.Services.AddTransient<RepositoryHospital>();
+builder.Services.AddDbContext<HospitalContext>
+    (options => options.UseSqlServer(connectionString));
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    
+}
+app.MapOpenApi();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSwaggerUI(options => {
+
+    options.SwaggerEndpoint("/openapi/v1.json", "Primera Api con Seguridad");
+    options.RoutePrefix = "";
+});
+
+app.MapControllers();
+
+app.Run();
